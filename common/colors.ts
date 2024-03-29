9e88b678199e5aec38febe4fea7dc32f5af80784
immails <anton140315@gmail.com>
2024-03-29 23:14:31 +0700

    1.0.49

--- common/colors.ts
+++ common/colors.ts
@@ -7,7 +7,8 @@ export const Colors = {
     "SYS": {
         "RESET": "\x1b[0m",
         "BRIGHT": "\x1b[1m",
-        "DIM": "\x1b[2m"
+        "DIM": "\x1b[2m",
+        "UNDERLINE": "\x1b[4m"
     },
     /** Foreground colors */
     "FG": {
